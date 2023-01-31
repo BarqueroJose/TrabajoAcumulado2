11d1880618b50669e3ff32d0075651740fd12830
BarqueroJose <josebarquerolazo12@gmail.com>
2023-01-30 21:20:17 -0600

    Cargando Trabajo

---
+++ src/main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author barquero
+ */
+public class main {
+    public static void main(String[] args) {
+        MdiPrincipal principal = new MdiPrincipal();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+        
+    }
+}
